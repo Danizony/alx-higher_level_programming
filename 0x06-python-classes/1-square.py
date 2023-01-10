a92d9a420d1d7afa3c91433512c6acfcb72d893a
Danizony <danzin2000@example.com>
2023-01-10 12:01:10 -0800

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,12 @@
- #!/usr/bin/python3
- """ Working on oop with python """
+#!/usr/bin/python3
+""" Working on oop with python """
 
 
- class Square():
+class Square():
     """Class representing a square"""
 
     def __init__(self, size):
         """ initialize the class
-
         Args:
            size (int): size of the square
         """
