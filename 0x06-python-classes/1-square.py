7c098b19f7e02ae72136689d8939915d7d17222b
Danizony <danzin2000@example.com>
2023-01-10 09:00:20 -0800

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Program with a class that defines a square based on some conditions"""
+
+
+class Square():
+   """square class with private attribute"""
+
+   def __init__(self, size):
+       """Initialize the class
+
+       Args:
+            size (int): size of the square
+       """
+       self.__size = size
+
