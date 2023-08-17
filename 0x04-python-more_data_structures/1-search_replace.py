c2c15f9e0b56523161ce1185d124da2a124a4b11
Danizony <danzin2000@gmail.com>
2023-08-16 20:51:54 -0700

    fixed an error

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    new_list = [item if item != search else replace for element in my_list]
+    new_list = [item if item != search else replace for item in my_list]
     return new_list
