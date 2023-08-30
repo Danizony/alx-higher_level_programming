c022d0f154e414ccf2ea300f1bfe90f0aeb653c3
Danizony <danzin2000@gmail.com>
2023-08-29 20:21:51 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Defines a square"""
+
+
+class Square:
+    """The class represents a square"""
+
+    def __init__(self, size):
+        """Initializing the square class
+        Args: size - for the size of the square
+        """
+
+        self.__size = size
