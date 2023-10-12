31699151e5bf4c7ce28dbe6e7b167c86e08520f1
Daniel Zongo <danzin2000@gmail.com>
2023-10-12 14:56:40 +0100

    Delete 0x0A-python-inheritance directory

--- 0x0A-python-inheritance/101-add_attribute.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""This module defines a function that adds attributes to objects"""
-
-
-def add_attribute(obj, attr, value):
-    """adds new attribute to an object"""
-    if not hasattr(obj, "__dict__"):
-        raise TypeError("can't add new attribute")
-    setattr(obj, attr, value)
--- 0x0A-python-inheritance/5-base_geometry.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Module containing an empty class"""
-
-
-class BaseGeometry:
-    """Empty class that contains nothing"""
-    pass
