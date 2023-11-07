9a8c4c07c75c8f4bc24c178aad7aa6f2f3ee755d
Daniel Zongo <danzin2000@gmail.com>
2023-11-06 19:36:03 -0800

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a class BaseGeometry."""
+
+
+class BaseGeometry:
+    """This class represents the base geometry"""
+    
+    def area(self):
+        """An unimplemented method"""
+        raise Exception("area() is not implemented")
