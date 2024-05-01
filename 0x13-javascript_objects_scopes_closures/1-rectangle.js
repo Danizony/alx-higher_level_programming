1157f1d6f845e0e2202a22322605e635dad47d4f
danizony <danzin2000@gmail.com>
2024-04-30 17:29:55 -0700

    error fixed

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /**
- * This represents a rectangle class with width and height
- * attributes
+ * This represents a rectangle class
+ * with width and height attributes
  */
 class Rectangle {
   constructor (w, h) {
