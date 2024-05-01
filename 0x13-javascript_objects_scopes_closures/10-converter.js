3110a5d6c789cd4c0a7f71695fbae60430fb6d92
danizony <danzin2000@gmail.com>
2024-05-01 13:53:16 -0700

    corrected

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-/**
- * converts a number from base 10 to another 
- * base passed as argument.
- */ 
+
 exports.converter = function (base) {
   function myConverter (n) {
     return n.toString(base);
--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-/**
- * prints the number of arguments already 
- * printed and the new argument value.
- */
+
 let counter = 0;
 
 exports.logMe = function count (item) {
